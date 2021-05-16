7892bdf2cbca66c9913a3d1c703a4860a0cfbf85
Sebastien Moreau <sebastien.moreau@osedea.com>
2021-05-16 12:07:06 +0200

    Fixed type issue

--- lib/variosound.dart
+++ lib/variosound.dart
@@ -2,27 +2,27 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Variosound {
-  static const MethodChannel _channel =
-      const MethodChannel('variosound');
+  static const MethodChannel _channel = const MethodChannel('variosound');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-	static void play() {
-		_channel.invokeMethod('play');
-	}
+  static void play() {
+    _channel.invokeMethod('play');
+  }
 
-	static void stop() {
-		_channel.invokeMethod('stop');
-	}
+  static void stop() {
+    _channel.invokeMethod('stop');
+  }
 
-	static Future<bool> get isPlaying {
-		return _channel.invokeMethod('isPlaying') as Future<bool>;
-	}
+  static Future<bool> get isPlaying async {
+    final dynamic isPlaying = await _channel.invokeMethod('isPlaying');
+    return isPlaying as bool;
+  }
 
-	static void setSpeed(double speed) {
-		_channel.invokeMethod('setSpeed', <String, dynamic>{ 'speed': speed });
-	}
+  static void setSpeed(double speed) {
+    _channel.invokeMethod('setSpeed', <String, dynamic>{'speed': speed});
+  }
 }
